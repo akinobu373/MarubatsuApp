2a0a585a05fd82495b401ff6824c0a5d3625c931
akinobu <siguiente.football.373@gmail.com>
2021-11-10 14:20:05 +0900

    課題修正

--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     var currentQuestionNum: Int = 0
     var questions: [[String:Any]] = []
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         let userDefaults = UserDefaults.standard
